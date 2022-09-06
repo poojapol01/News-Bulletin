2db1e73fefc89ed61af45bff0e497a7ba07d5a72
poojapol01 <polpooja05@gmail.com>
2022-09-06 23:54:09 +0530

    Added Volley library and MySingleton Class

--- app/src/main/java/com/example/newsbulletin/MainActivity.java
+++ app/src/main/java/com/example/newsbulletin/MainActivity.java
@@ -8,6 +8,13 @@ import java.util.Arrays;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     // ArrayList for person names
@@ -25,5 +32,33 @@ public class MainActivity extends AppCompatActivity {
         //  call the constructor of CustomAdapter to send the reference and data to Adapter
         NewsListAdapter newsListAdapter = new NewsListAdapter(MainActivity.this, list);
         recyclerView.setAdapter(newsListAdapter); // set the Adapter to RecyclerView
+
+
+        //API = 2c80b95cd2384627adc499fe8bb7d586
+    }
+    private void fetchData(){
+        String url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2c80b95cd2384627adc499fe8bb7d586";
+
+        JsonObjectRequest
+                jsonObjectRequest
+                = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener() {
+                    @Override
+                    public void onResponse(Object response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                    }
+                });
+        //requestQueue.add(jsonObjectRequest);
+        // Add StringRequest to the RequestQueue
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 }
